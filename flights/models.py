194a24f0791c0135e297ee95879c1d19b3948ccf
Ali <alishabaniara123@gmail.com>
2025-03-11 11:51:12 +0330

    add model

--- flights/models.py
+++ flights/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Flight(models.Model):
+         origin = models.CharField(max_length=100)
+         destination = models.CharField(max_length=100)
+         time = models.CharField(max_length=50)
+         price = models.CharField(max_length=50)
+         capacity = models.CharField(max_length=50)
+         type_of_class = models.CharField(max_length=50)
+         departure_date = models.DateField()  
+         request_date_time = models.DateTimeField(default=timezone.now)  
+         def str(self):
+             return f"{self.origin} to {self.destination} at {self.time}"
+
+class Bus(models.Model):
+         origin = models.CharField(max_length=100)
+         destination = models.CharField(max_length=100)
+         time = models.CharField(max_length=50)
+         price = models.CharField(max_length=50)
+         capacity = models.CharField(max_length=50)
+         type_of_class = models.CharField(max_length=50)
+         departure_date = models.DateField()  
+         request_date_time = models.DateTimeField(default=timezone.now)  
+         def str(self):
+             return f"{self.origin} to {self.destination} at {self.time}"
+
+class Train(models.Model):
+         origin = models.CharField(max_length=100)
+         destination = models.CharField(max_length=100)
+         time = models.CharField(max_length=50)
+         price = models.CharField(max_length=50)
+         capacity = models.CharField(max_length=50)
+         type_of_class = models.CharField(max_length=50)
+         departure_date = models.DateField()  
+         request_date_time = models.DateTimeField(default=timezone.now)  
+         def str(self):
+             return f"{self.origin} to {self.destination} at {self.time}"
