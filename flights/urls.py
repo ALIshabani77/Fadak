8f1889c5ffec2e04c0d435a0a7387357a0926fa8
Ali <alishabaniara123@gmail.com>
2025-03-11 11:19:54 +0330

    add url

--- flights/urls.py
+++ flights/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from .views import FlightList
+from .views import Buslist
+from .views import Trainlist
+
 
 urlpatterns = [
+
          path('api/flights/<str:origin_destination>/<str:date>/', FlightList.as_view(), name='flight_list'),
-     ]
+         path('api/buses/<str:origin_destination>/<str:date>/', Buslist.as_view(), name='bus_list'), 
+         path('api/trains/<str:origin_destination>/<str:date>/', Trainlist.as_view(), name='train_list'), 
+         
+              ]
 
 
 
