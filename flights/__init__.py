2a1b65d2512d3f087764eda0cb8bcab50231e13e
Ali <alishabaniara123@gmail.com>
2025-04-21 18:17:11 +0330

    add content to __init__.py file for the flights app

---
+++ flights/__init__.py
@@ -0,0 +1,10 @@
+# ---FILEPATH flights/__init__.py
+# ---FIND
+# ```
+# # This is the __init__.py file for the flights app
+# ```
+# ---REPLACE
+# ```
+# # This is the __init__.py file for the flights app
+# ```
+# ---COMPLETE
